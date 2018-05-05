1b4a9d53458638d95bce241525be07b46b7896c6
Fiona0730pku <37998805+Fiona0730pku@users.noreply.github.com>
2018-05-05 23:49:22 +0800

    Update Fourier.py
    add lowpass filtering

--- Fourier.py
+++ Fourier.py
@@ -2,12 +2,23 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('1.jpg',0) #直接读为灰度图像
-f = np.fft.fft2(img)
-fshift = np.fft.fftshift(f)
-#取绝对值：将复数变化成实数
-#取对数的目的为了将数据变化到较小的范围（比如0-255）
-s1 = np.log(np.abs(f))
-s2 = np.log(np.abs(fshift))
-plt.subplot(121),plt.imshow(s1,'gray'),plt.title('original'),plt.show()
-plt.subplot(122),plt.imshow(s2,'gray'),plt.title('center'),plt.show()
+img_man = cv2.imread('test2.jpg',0) #直接读为灰度图像
+plt.subplot(121),plt.imshow(img_man,'gray'),plt.title('origial')
+plt.xticks([]),plt.yticks([])
+#--------------------------------
+rows,cols = img_man.shape
+mask = np.zeros(img_man.shape,np.uint8)
+mask[(int)(rows/2-20):(int)(rows/2+20),(int)(cols/2-20):(int)(cols/2+20)] = 1
+#--------------------------------
+f1 = np.fft.fft2(img_man)
+f1shift = np.fft.fftshift(f1)
+f1shift = f1shift*mask
+f2shift = np.fft.ifftshift(f1shift) #对新的进行逆变换
+img_new = np.fft.ifft2(f2shift)
+#出来的是复数，无法显示
+img_new = np.abs(img_new)
+#调整大小范围便于显示
+img_new = (img_new-np.amin(img_new))/(np.amax(img_new)-np.amin(img_new))
+plt.subplot(122),plt.imshow(img_new,'gray'),plt.title('Lowpass')
+plt.xticks([]),plt.yticks([])
+plt.show()
